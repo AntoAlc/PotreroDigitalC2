cc77dc6e6d290e660645470bc741eb9ddea61763
AntoAlc <antoo.alcaraz@gmail.com>
2022-07-04 21:06:18 -0300

    [ADD] TP JS - Clase 9

---
+++ js/execute.js
@@ -0,0 +1,49 @@
+function copyright() {
+  Swal.fire(
+    "Info",
+    "Sitio creado por Antonella Alcaraz para\n POTRERO DIGITAL, \ncontacto: antoo.alcaraz@gmail.com",
+    "success"
+  );
+}
+
+
+function modoOscuro()
+    {
+
+        document.body.style.backgroundImage= "none";
+        document.body.style.backgroundColor= "#000000";
+        document.getElementById('BlurFondo').style.backgroundColor="#000000";
+        document.body.style.color="#ffff";
+
+        document.getElementById("header").style.background="none";
+        
+        document.getElementById("kangrebur").style.color = "#ffff";;
+        document.getElementById("cambiarModo").style.color = "#ffff";;
+        document.getElementById("h1").style.color = "#ffff";;
+        document.getElementById("GoTo").style.color = "#ffff";;
+        document.getElementById("nav").style.background = "#484848";;
+        document.getElementById("nav").style.color = "#ffff";;
+        document.getElementById("titleMap").style.color = "#ffff";;
+        document.getElementById("TextAudio").style.color = "#ffff";;
+        document.getElementById("TextAudio2").style.color = "#ffff";;
+        document.getElementById("cardQuote").style.background="#252222";
+
+        document.getElementById("cardb1").style.background = "#252222";;
+        document.getElementById("cardb2").style.background = "#252222";;
+        document.getElementById("cardb3").style.background = "#252222";;
+        document.getElementById("cardb4").style.background = "#252222";;
+        document.getElementById("TitleCards").style.color = "#ffff";;
+        
+        document.getElementById("footerPage").style.background = "#252222";;
+        
+        //document.getElementsByTagName("a").style.color="white";
+
+        //document.getElementByClassName("main").style.backgroundColor = "black";
+       //document.getElementsById("header").style.color = "#90ffe9";
+        //document.getElementsById("nav").style.backgroundColor = "whitesmoke";
+        //document.getElementsById("nav").style.color = "black";
+        // document.getElementById("section").style.backgroundColor = "black";
+        // document.getElementById("section").style.color = "whitesmoke";
+        // document.getElementById("aside").style.backgroundColor = "black";
+        // document.getElementById("footer").style.backgroundColor = "black";
+    }
---
+++ js/welcome.js
@@ -0,0 +1,15 @@
+Swal.fire({
+    title: '¡BIENVENIDO!',
+    text:  '¿Preparado para no parar de cocinar?',
+    padding: '1em',
+    color: '#499954',
+    background: '#fff url("https://media.diy.com/is/image/KingfisherDigital/galerie-kitchen-style-3-grey-wood-panelling-smooth-wallpaper~0058559366153_01c_MP?wid=256&hei=256")',
+
+    backdrop: `
+      rgba(255, 255, 255)
+      url("https://cdn.dribbble.com/users/2574702/screenshots/6549234/kitchen.gif")
+      left top
+      no-repeat
+      
+    `
+  })
